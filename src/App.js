911c651d46b61bd99c9c41f795f2b9ed3d8dd48c
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-05-01 18:01:06 -0500

    react-router-dom with outlet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link, Outlet } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+
+function Layout() {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                </ul>
+            </nav>
+            <Outlet />
+        </div>
+    )
+}
 
 export default function App() {
     return (
-        <div>IT IS ALIVE!</div>
+        <Router>
+            <div>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                    </Route>
+                </Routes>
+            </div>
+        </Router>
     )
 }
